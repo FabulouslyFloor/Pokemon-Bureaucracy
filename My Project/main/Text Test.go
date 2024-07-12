386e31a2d13819c9174e620414071ca1c136feb2
FabulouslyFloor <113189357+FabulouslyFloor@users.noreply.github.com>
2024-07-12 17:04:57 -0400

    The Pictures :Cri:

--- My Project/main/Text Test.go
+++ My Project/main/Text Test.go
@@ -14,7 +14,7 @@ components {
   }
 }
 embedded_components {
-  id: "label"
+  id: "pokemon1Name"
   type: "label"
   data: "size {\n"
   "  x: 128.0\n"
@@ -50,8 +50,102 @@ embedded_components {
   "material: \"/builtins/fonts/label-df.material\"\n"
   ""
   position {
+    x: -250.0
+    y: 400.0
+    z: 0.0
+  }
+  rotation {
     x: 0.0
     y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "pokemon1"
+  type: "sprite"
+  data: "default_animation: \"1-1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/Art/PokemonSprites.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: -250.0
+    y: 250.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "pokemon2"
+  type: "sprite"
+  data: "default_animation: \"1-2\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/Art/PokemonSprites.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 250.0
+    y: 250.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "pokemon2Name"
+  type: "label"
+  data: "size {\n"
+  "  x: 128.0\n"
+  "  y: 32.0\n"
+  "  z: 0.0\n"
+  "  w: 0.0\n"
+  "}\n"
+  "color {\n"
+  "  x: 1.0\n"
+  "  y: 1.0\n"
+  "  z: 1.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "outline {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "shadow {\n"
+  "  x: 0.0\n"
+  "  y: 0.0\n"
+  "  z: 0.0\n"
+  "  w: 1.0\n"
+  "}\n"
+  "leading: 1.0\n"
+  "tracking: 0.0\n"
+  "pivot: PIVOT_CENTER\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "line_break: false\n"
+  "text: \"Label\"\n"
+  "font: \"/builtins/fonts/default.font\"\n"
+  "material: \"/builtins/fonts/label-df.material\"\n"
+  ""
+  position {
+    x: 250.0
+    y: 400.0
     z: 0.0
   }
   rotation {
